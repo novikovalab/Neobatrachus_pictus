b4379d079739b6701fc5f43fb0a4751a95105244
Jie Wang <76740046+wangjie07070910@users.noreply.github.com>
2025-11-21 04:04:09 +0100

    Pymol for the SYCE2 TEX12 2:2 complex

---
+++ protein modelling pymol.r
@@ -0,0 +1,21 @@
+####pymol
+bg_color white
+
+set_color SYCE2_blue, [0.59, 0.73, 0.86]
+set_color SYCE2_grey, [0.88, 0.88, 0.88]
+
+color SYCE2_blue, chain A and fold_2025_08_21_20_32_model_0
+set_color mygrey, [0.8, 0.8, 0.8]
+color mygrey, chain B and fold_2025_08_21_20_32_model_0
+
+
+color blue, chain B and fold_2025_08_21_20_42_model_1
+color grey, chain A and fold_2025_08_21_20_42_model_1
+
+set label_size, 18, measure01
+set label_placement_offset, 12, measure01
+
+set dash_color, yellow, measure01
+set dash_width, 3, measure01
+set dash_gap,   0.25, measure01
+set dash_length,0.6, measure01
